8dc951e4242004fcc00f887abf9a5ea6fabd0a31
Sanim G <gharbisanim@gmail.com>
2024-10-09 11:26:03 +0200

    fin tp 0

--- tp0/tp.c
+++ tp0/tp.c
@@ -12,9 +12,53 @@ void tp() {
 
    multiboot_memory_map_t* entry = (multiboot_memory_map_t*)info->mbi->mmap_addr;
    while((uint32_t)entry < (info->mbi->mmap_addr + info->mbi->mmap_length)) {
-      // TODO print "[start - end] type" for each entry
 
+      // TODO print "[start - end] type" for each entry
+      debug("[0x%llx - 0x%llx] ", entry->addr, entry->addr+entry->len);
+
+      if(entry->type==MULTIBOOT_MEMORY_AVAILABLE)
+      {
+         debug("MULTIBOOT_MEMORY_AVAILABLE\n");
+      }
+      else if(entry->type==MULTIBOOT_MEMORY_RESERVED)
+      {
+         debug("MULTIBOOT_MEMORY_RESERVED\n");
+      }
+      else if(entry->type==MULTIBOOT_MEMORY_ACPI_RECLAIMABLE)
+      {
+         debug("MULTIBOOT_MEMORY_ACPI_RECLAIMABLE\n");
+
+      }
+      else if(entry->type==MULTIBOOT_MEMORY_NVS)
+      {
+         debug("MULTIBOOT_MEMORY_NVS\n");
+      }
+      else
+      {
+         debug("entry type not known\n");
+      }
       entry++;
    }
+
+   int *ptr_in_available_mem;
+   ptr_in_available_mem = (int*)0x0;
+   debug("Available mem (%p): before: 0x%x ",ptr_in_available_mem, *ptr_in_available_mem); // read
+   *ptr_in_available_mem = 0xaaaaaaaa;                           // write
+   debug("after: 0x%x\n", *ptr_in_available_mem);                // check
+
+   int *ptr_in_reserved_mem;
+   ptr_in_reserved_mem = (int*)0x9fc00;
+   debug("Reserved mem (at: %p):  before: 0x%x ",ptr_in_reserved_mem, *ptr_in_reserved_mem); // read
+   *ptr_in_reserved_mem = 0xaaaaaaaa;                           // write
+   debug("after: 0x%x\n", *ptr_in_reserved_mem);                // check
+
+   int *ptr_after_physical_mem;
+   ptr_after_physical_mem = (int*)0xffffffffffffffff;
+   debug("Ptr after physcial mem (at: %p):  before: 0x%x ",ptr_after_physical_mem, *ptr_after_physical_mem); // read
+   *ptr_after_physical_mem = 0xaaaaaaaa;                           // write
+   debug("after: 0x%x\n", *ptr_after_physical_mem);                // check
+
+
+
 
 }
