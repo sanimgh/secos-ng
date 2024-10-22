911f82d091eab2a0a0128c1f19699a43cae321e4
Sanim G <gharbisanim@gmail.com>
2024-10-23 00:26:34 +0200

    segment descriptor types

--- tp1/tp.c
+++ tp1/tp.c
@@ -57,16 +57,33 @@ void tp() {
     seg_desc_t segment_descriptor[8];
 
 
-    segment_descriptor[0].raw=0;
-
-    segment_descriptor[1].s=0;
-    segment_descriptor[1].type=0b1010;
-    segment_descriptor[1].dpl=0;
-
-    segment_descriptor[2].s=1;
-    segment_descriptor[2].type=0b0010;
-    segment_descriptor[2].dpl=0;
-
+    segment_descriptor[0].raw = 0;
+    segment_descriptor[1].limit_1 = 0xffff;   //:16;     /* bits 00-15 of the segment limit */
+    segment_descriptor[1].base_1 = 0x0000;    //:16;     /* bits 00-15 of the base address */
+    segment_descriptor[1].base_2 = 0x00;      //:8;      /* bits 16-23 of the base address */
+    segment_descriptor[1].type = 11;//Code,RX //:4;      /* segment type */
+    segment_descriptor[1].s = 1;              //:1;      /* descriptor type */
+    segment_descriptor[1].dpl = 0; //ring0    //:2;      /* descriptor privilege level */
+    segment_descriptor[1].p = 1;              //:1;      /* segment present flag */
+    segment_descriptor[1].limit_2 = 0xf;      //:4;      /* bits 16-19 of the segment limit */
+    segment_descriptor[1].avl = 1;            //:1;      /* available for fun and profit */
+    segment_descriptor[1].l = 0; //32bits     //:1;      /* longmode */
+    segment_descriptor[1].d = 1;              //:1;      /* default length, depend on seg type */
+    segment_descriptor[1].g = 1;              //:1;      /* granularity */
+    segment_descriptor[1].base_3 = 0x00;      //:8;      /* bits 24-31 of the base address */
+    segment_descriptor[2].limit_1 = 0xffff;   //:16;     /* bits 00-15 of the segment limit */
+    segment_descriptor[2].base_1 = 0x0000; //avant 0x0000    //:16;     /* bits 00-15 of the base address */
+    segment_descriptor[2].base_2 = 0x00;      //:8;      /* bits 16-23 of the base address */
+    segment_descriptor[2].type = 3; //data,RW //:4;      /* segment type */
+    segment_descriptor[2].s = 1;              //:1;      /* descriptor type */
+    segment_descriptor[2].dpl = 0; //ring0    //:2;      /* descriptor privilege level */
+    segment_descriptor[2].p = 1;              //:1;      /* segment present flag */
+    segment_descriptor[2].limit_2 = 0xf;      //:4;      /* bits 16-19 of the segment limit */
+    segment_descriptor[2].avl = 1;            //:1;      /* available for fun and profit */
+    segment_descriptor[2].l = 0; // 32 bits   //:1;      /* longmode */
+    segment_descriptor[2].d = 1;              //:1;      /* default length, depend on seg type */
+    segment_descriptor[2].g = 1;              //:1;      /* granularity */
+    segment_descriptor[2].base_3 = 0x00;      //:8;      /* bits 24-31 of the base address */
     gdt_reg_t gdtr;
     gdtr.limit=sizeof(segment_descriptor)-1;
     gdtr.desc=segment_descriptor;
