70329dfde575b3cd7440c20b1bfba2e47662d2d8
Sanim G <gharbisanim@gmail.com>
2024-12-19 12:59:00 +0100

    fix du probleme des interruptions

--- tp_exam/tp.c
+++ tp_exam/tp.c
@@ -75,39 +75,15 @@ uint32_t  task2_stack_base = (uint32_t)&task2_stack[STACK_SIZE-1];
 #define INTERRUPT_STACK_TASK_1 0x380000
 #define INTERRUPT_STACK_TASK_2 0x390000
 
-void ring_0_to_ring_3_task(uint32_t esp, uint32_t interrupt_stack, pde32_t* pgd, void * entrypoint)
-{
-    set_ds(d3_sel);
-    set_es(d3_sel);
-    set_fs(d3_sel);
-    set_gs(d3_sel);
-	TSS.s0.esp = interrupt_stack;
-	set_cr3(pgd);
-    asm volatile (
-      "push %0 \n" // ss
-      "push %1 \n" // esp pour du ring 3 !
-      "pushf   \n" // eflags
-      "push %2 \n" // cs
-      "push %3 \n" // eip
-      ::
-       "i"(d3_sel),
-       "m"(esp),
-       "i"(c3_sel),
-       "r"(entrypoint)
-      );
-	asm volatile("iret");
-}
-
 /* by default functions are mapped in kernel space */
 __attribute__((section(".task1_code")))
 void task1_function()
 {
 	uint32_t * counter = (uint32_t*)0x800000;
-	*counter=1;
+	*counter=0;
 	while(1)
 	{
 		*counter = *counter + 1;
-		asm volatile("int $32");
 	}
 }
 
@@ -118,7 +94,6 @@ void task2_function()
 	while(1)
 	{
         asm volatile("int $35");// dipslay virtual adress 0x400000, debug(print) is in kernel space
-		asm volatile("int $32");
 	}
 }
 
@@ -152,7 +127,16 @@ void clock_interrupt_isr() {
 
 
 void __regparm__(1) clock_interrupt_handler(int_ctx_t *ctx) {
+
 	int next_task = (current_task_idx + 1) % 2;
+	if(next_task==1)
+	{
+		debug("Printing Counter... \n");
+	}
+	else
+	{
+		debug("Increasing Counter... \n");
+	}
 
 	memcpy(&tasks[current_task_idx].context,ctx,sizeof(int_ctx_t));
 	tasks[current_task_idx].context_saved=1;
@@ -174,14 +158,15 @@ void __regparm__(1) clock_interrupt_handler(int_ctx_t *ctx) {
    {
 
 		asm volatile(
+			"sti \n"
 			"push %0 \n" // ss
 			"push %1 \n" // esp
 			"pushf   \n" // eflags
 			"push %2 \n" // cs
 			"push %3 \n" // eip
-			::"i"(d0_sel),
+			::"i"(d3_sel),
 			"m"(tasks[next_task].stack),
-			"i"(c0_sel),
+			"i"(c3_sel),
 			"r"(tasks[next_task].function));
 		outb(32, 32);
 		asm volatile("sti ; iret");
@@ -267,13 +252,14 @@ void config_pgds()
 
 
 	set_cr3((uint32_t)pgd);
+
 	uint32_t cr0 = get_cr0();
 	set_cr0(cr0|CR0_PG);
 
 	debug("pagination enabled\n");
 }
 
-void config_clock_interrupt()
+void config_interrupt()
 {
   	int_desc_t *dsc;
 	idt_reg_t  idtr;
@@ -303,9 +289,28 @@ void config_task(struct task *task, void *func, uint32_t stack,uint32_t interrup
 void config() {
 	config_segmentation();
 	config_pgds();
-	config_clock_interrupt();
+	config_interrupt();
     config_task(&tasks[0], task1_function, task1_stack_base,INTERRUPT_STACK_TASK_1+INTERRUPT_STACK_SIZE ,pgd_task_1);
     config_task(&tasks[1], task2_function, task2_stack_base,INTERRUPT_STACK_TASK_2+INTERRUPT_STACK_SIZE ,pgd_task_2);
-	force_interrupts_on();
-	ring_0_to_ring_3_task(tasks[0].stack,tasks[0].interrupt_stack,tasks[0].pgd,tasks[0].function);
+
+	set_ds(d3_sel);
+    set_es(d3_sel);
+    set_fs(d3_sel);
+    set_gs(d3_sel);
+	TSS.s0.esp = tasks[0].interrupt_stack;
+	set_cr3((uint32_t)tasks[0].pgd);
+	asm volatile (
+	  "sti \n"
+      "push %0 \n" // ss
+      "push %1 \n" // esp pour du ring 3 !
+      "pushf   \n" // eflags
+      "push %2 \n" // cs
+      "push %3 \n" // eip
+      ::
+       "i"(d3_sel),
+       "m"(tasks[0].stack),
+       "i"(c3_sel),
+       "r"(tasks[0].function)
+      );
+	asm volatile("iret");
 }
